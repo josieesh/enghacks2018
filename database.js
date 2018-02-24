298ff52085803367d547344232299e56ac97ea11
josieesh <eshkenazi.josephine@gmail.com>
2018-02-24 14:08:31 -0500

    first commit

---
+++ database.js
@@ -0,0 +1,38 @@
+// Require the nedb module
+var Datastore = require('nedb'),
+    fs = require('fs');
+
+// Initialize two nedb databases. Notice the autoload parameter.
+var dilemmas = new Datastore({ filename: __dirname + '/data/dilemmas', autoload: true }),
+    users = new Datastore({ filename: __dirname + '/data/users', autoload: true });
+
+// Create a "unique" index for the photo name and user ip
+dilemmas.ensureIndex({fieldName: 'name', unique: true});
+users.ensureIndex({fieldName: 'ip', unique: true});
+
+// Load all images from the public/photos folder in the database
+var dilemmas_on_disk = fs.readdirSync(__dirname + '/public/dilemmas');
+
+// Insert the photos in the database. This is executed on every
+// start up of your application, but because there is a unique
+// constraint on the name field, subsequent writes will fail
+// and you will still have only one record per image:
+
+dilemmas_on_disk.forEach(function(dilemma){
+    dilemmas.insert({
+        name: dilemma,
+        a: 0,
+        b: 0,
+        c: 0,
+        d: 0,
+        e: 0
+    });
+});
+
+// Make the photos and users data sets available to the code
+// that uses require() on this module:
+
+module.exports = {
+    dilemmas: dilemmas,
+    users: users
+};
---
+++ routes.js
@@ -0,0 +1,117 @@
+/**
+ * This file defines the routes used in your application
+ * It requires the database module that we wrote previously.
+ */
+
+var db = require('./database'),
+    dilemmas = db.dilemmas,
+    users = db.users;
+
+module.exports = function(app){
+
+    // Homepage
+    app.get('/', function(req, res){
+
+        // Find all dilemmas
+        dilemmas.find({}, function(err, all_dilemmas){
+
+            // Find the current user
+            users.find({ip: req.ip}, function(err, u){
+
+                var voted_on = [];
+
+                if(u.length == 1){
+                    voted_on = u[0].votes;
+                }
+
+                // Find which dilemmas the user hasn't still voted on
+
+                var not_voted_on = all_dilemmas.filter(function(dilemma){
+                    return voted_on.indexOf(dilemma._id) == -1;
+                });
+
+                var dilemma_to_show = null;
+
+                if(not_voted_on.length > 0){
+                    // Choose a random dilemma from the array
+                    dilemma_to_show = not_voted_on[Math.floor(Math.random()*not_voted_on.length)];
+                }
+
+                res.render('home', { dilemma: dilemma_to_show });
+
+            });
+
+        });
+
+    });
+
+    app.get('/standings', function(req, res){
+
+        dilemmas.find({}, function(err, all_dilemmas){
+
+            // Sort the dilemmas
+
+            /*all_dilemmas.sort(function(p1, p2){
+                return (p2.likes - p2.dislikes) - (p1.likes - p1.dislikes);
+            });*/
+
+            // Render the standings template and pass the dilemmas
+            res.render('standings', { standings: all_dilemmas });
+
+        });
+
+    });
+
+    // This is executed before the next two post requests
+    app.post('*', function(req, res, next){
+
+        // Register the user in the database by ip address
+
+        users.insert({
+            ip: req.ip,
+            votes: []
+        }, function(){
+            // Continue with the other routes
+            next();
+        });
+
+    });
+
+    app.post('/a', vote);
+    app.post('/b', vote);
+    app.post('/c', vote);
+    app.post('/d', vote);
+    app.post('/e', vote);
+
+    function vote(req, res){
+
+        // Which field to increment, depending on the path
+
+        var what = {
+            '/a': {a:1},
+            '/b': {b:1},
+            '/c': {c:1},
+            '/d': {d:1},
+            '/e': {e:1}
+        };
+
+        // Find the dilemma, increment the vote counter and mark that the user has voted on it.
+
+        dilemmas.find({ name: req.body.dilemma }, function(err, found){
+
+            if(found.length == 1){
+
+                dilemmas.update(found[0], {$inc : what[req.path]});
+
+                users.update({ip: req.ip}, { $addToSet: { votes: found[0]._id}}, function(){
+                    res.redirect('../');
+                });
+
+            }
+            else{
+                res.redirect('../');
+            }
+
+        });
+    }
+  };
